70f7a9c4f92925fb45ccf8e86eb715e73128dcf7
Davi_conti <davi_conti@estudante.sesisenai.org.br>
2025-11-12 08:40:52 -0300

    na tela index tem um unico erro

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ require_once 'conexao.php';
 
 $sql = "SELECT * FROM clientes";
 $result = mysqli_query($conn, $sql);
-
 ?>
 <!doctype html>
 <html>
@@ -17,14 +16,17 @@ $result = mysqli_query($conn, $sql);
   <table border="1" cellpadding="5" cellspacing="0">
     <tr><th>ID</th><th>Nome</th><th>CPF</th><th>Ações</th></tr>
     <?php
-
-    if ($results) {
-      while ($row = mysqli_fetch_assoc($results)) {
+    // ❌ ERRO: variável usada era $results (com 's'), mas a correta é $result (sem 's')
+    if ($result && mysqli_num_rows($result) > 0) {
+      while ($row = mysqli_fetch_assoc($result)) {
         echo '<tr>';
-        echo '<td>' . $row['id'] . '</td>';
-        echo '<td>' . $row['nome'] . '</td>';
-        echo '<td>' . $row['cpf'] . '</td>';
-        echo '<td><a href="editar.php?id=' . $row['id'] . '">Editar</a> | <a href="excluir.php?id=' . $row['id'] . '">Excluir</a></td>';
+        echo '<td>' . htmlspecialchars($row['id']) . '</td>';
+        echo '<td>' . htmlspecialchars($row['nome']) . '</td>';
+        echo '<td>' . htmlspecialchars($row['cpf']) . '</td>';
+        echo '<td>
+                <a href="editar.php?id=' . $row['id'] . '">Editar</a> | 
+                <a href="excluir.php?id=' . $row['id'] . '">Excluir</a>
+              </td>';
         echo '</tr>';
       }
     } else {
